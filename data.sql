57881ab9efc0afab833c107300f97abfcadd840c
PraisesPJMT <praisesmusa@outlook.com>
2022-10-17 16:06:23 +0100

    Add data.sql file - File contains code for making four (4) value enteries into the `animals` table of the database

---
+++ data.sql
@@ -0,0 +1,9 @@
+/* Inserting four (4) values into 'animals' table
+Each value having id, name, date_of_birth,
+escape_attempts, neutered,  and weight_kg
+*/
+
+insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) values (1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
+insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) values (2, 'Gabumon', '2018-11-15', 2, TRUE, 8);
+insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) values (3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04);
+insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) values (4, 'Devimon', '2017-05-12', 5, TRUE, 11);
