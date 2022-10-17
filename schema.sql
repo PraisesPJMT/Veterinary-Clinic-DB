36fd8b6f9efb86d7ea572984a91d49e8523707ce
PraisesPJMT <praisesmusa@outlook.com>
2022-10-17 16:05:17 +0100

    Add schema.sql file File contains code to - Create the `vet clinic` database - Create the `animals` table

---
+++ schema.sql
@@ -0,0 +1,22 @@
+/* Vet Clinic Database
+*/
+
+-- Creating a database called 'vet clinic'
+CREATE DATABASE vet_clinic;
+
+-- Connecting to 'vet_clinic' database
+\c vet_clinic;
+
+/* Creating 'Animals' table in 'vet_clinic' database
+Table has columns: id, name, date_of_birth, escape_attempts,
+neutered,  and weight_kg
+*/
+CREATE TABLE animals (
+    id  INT,
+    name    VARCHAR(20),
+    date_of_birth   DATE,
+    escape_attempts INT,
+    neutered    BOOLEAN,
+    weight_kg   DECIMAL,
+    PRIMARY KEY(id)
+);
