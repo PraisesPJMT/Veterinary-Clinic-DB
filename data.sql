4cbf770ea25b38605268f76645d4644314b823b7
PraisesPJMT <praisesmusa@outlook.com>
2022-10-20 11:40:46 +0100

    Update data.sql file with code for adding data into vets, visits and specialties tables Includes codes for - Inserting four(4) data into the 'vets' table, Each data having a name, age, and date_of_graduation - Inserting three(3) data into the 'specialties' table, Each data having a specie, and vet - Inserting twenty(20) data into the 'visits' table, Each data having a animal, vet, and date_visited

--- data.sql
+++ data.sql
@@ -42,3 +42,32 @@ UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer
 UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon', 'Plantmon');
 UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon', 'Boarmon');
+
+/* Inserting four(4) data into the 'vets' table
+   Each data having a name, age, and date_of_graduation
+*/
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES  ('William Tatcher', 45, '2000-04-23'), ('Maisy Smith', 26, '2019-01-17'),
+        ('Stephanie Mendez', 64, '1981-05-04'), ('Jack Harkness', 38, '2008-06-08');
+
+/* Inserting three(3) data into the 'specialties' table
+   Each data having a specie, and vet
+*/
+INSERT INTO specialties (vet, specie)
+VALUES  ('William Tatcher', 'Pokemon'), ('Stephanie Mendez', 'Digimon'),
+        ('Stephanie Mendez', 'Pokemon'), ('Jack Harkness', 'Digimon');
+
+/* Inserting twenty(20) data into the 'visits' table
+   Each data having a animal, vet, and date_visited
+*/
+INSERT INTO visits (animal, vet, date_visited)
+VALUES  ('Boarmon', 'Maisy Smith', '2019-01-24'), ('Boarmon', 'Maisy Smith', '2019-05-15'),
+        ('Squirtle', 'Stephanie Mendez', '2019-09-29'), ('Plantmon', 'Maisy Smith', '2019-12-21'),
+        ('Pikachu', 'Maisy Smith', '2020-01-05'), ('Boarmon', 'Maisy Smith', '2020-02-27'),
+        ('Pikachu', 'Maisy Smith', '2020-03-08'), ('Pikachu', 'Maisy Smith', '2020-05-14'),
+        ('Agumon', 'William Tatcher', '2020-05-24'), ('Blossom', 'Stephanie Mendez', '2020-05-24'),
+        ('Agumon', 'Stephanie Mendez', '2020-07-22'), ('Boarmon', 'Maisy Smith', '2020-08-03'),
+        ('Plantmon', 'William Tatcher', '2020-08-10'), ('Angemon', 'Jack Harkness', '2020-10-03'),
+        ('Angemon', 'Jack Harkness', '2020-11-04'), ('Blossom', 'William Tatcher', '2021-01-11'),
+        ('Gabumon', 'Jack Harkness', '2021-02-02'), ('Charmander', 'Jack Harkness', '2021-02-24'),
+        ('Plantmon', 'Maisy Smith', '2021-04-07'), ('Devimon', 'Stephanie Mendez', '2021-05-04');
