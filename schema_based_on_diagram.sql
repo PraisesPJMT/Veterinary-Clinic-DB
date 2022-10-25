c5a6c33c30f3ca9db6e19c82549e8d9cae6929ab
PRATAP-KUMAR <pratap@fastmail.fm>
2022-10-25 22:30:17 +0530

    CREATE TABLES - INVOICES & INVOICES_ITEMS

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -19,9 +19,9 @@ CREATE TABLE patients(
 CREATE TABLE medical_histories(
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     admitted_at TIMESTAMP,
-    status VARCHAR(50),
     patient_id INT,
-
+    status VARCHAR(50),
+    
     CONSTRAINT fk_patient_id FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
 -- create a non clustered index for patient_id on medical_histories table
@@ -35,3 +35,36 @@ CREATE TABLE treatments(
     type VARCHAR(40),
     name VARCHAR(50)
 );
+
+/* Creating 'invoices' table in 'clinic' database
+   Table has columns: id, total_amount, generated_amount, payed_at, and medical_history_id(As Foreign Key)
+*/
+CREATE TABLE invoices(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL,
+    generated_amount TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT,
+
+    CONSTRAINT fk_medical_history_id FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
+);
+-- create a non clustered index for medical_history_id on invoices table
+CREATE INDEX invoices_medical_history_id ON invoices(medical_history_id);
+
+/* Creating 'invoice_items' table in 'clinic' database
+   Table has columns: id, unit_price, total_price, total_price,
+   quantity, invoice_id,(As Foreign Key) and treatment_id(As Foreign Key)
+*/
+
+CREATE TABLE invoice_items(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL,
+    total_price DECIMAL,
+    quantity INT,
+    invoice_id INT,
+    treatment_id INT,
+
+    CONSTRAINT fk_invoice_id FOREIGN KEY(invoice_id) REFERENCES invoices(id)
+);
+-- create a non clustered index for invoice_id on invoice_items table
+CREATE INDEX invoice_items_invoice_id ON invoice_items(invoice_id);
